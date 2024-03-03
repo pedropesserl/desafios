c89fa18cea18b945c88e2dc9f93c016cf4bba501
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-03-03 12:36:32 -0300

    finish first exercise

--- semana-1/a.cpp
+++ semana-1/a.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t;
-    std::cin >> t;
-    int x, y;
+#define llu long long unsigned
 
+int main() {
+    llu t;
+    string x, y;
+    cin >> t;
+    for (llu i = 0; i < t; i++) {
+        cin >> x >> y;
+        int last_x = (int)x[x.size() - 1] - '0';
+        int last_y = (int)y[y.size() - 1] - '0';
+        printf((last_x + last_y) % 2 == 0 ? "PARCEIROS\n" : "NAO PARCEIROS\n");
+    }
     return 0;
 }
