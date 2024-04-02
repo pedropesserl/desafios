31c62a929345da43cc8de9bcc2f117fd54ade73b
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-02 00:12:30 -0300

    [week 4] finish b

--- semana-4/b.cpp
+++ semana-4/b.cpp
@@ -4,31 +4,33 @@ using namespace std;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    size_t n, l, d, litros = 0;
+    size_t n;
     cin >> n;
-
-    map<size_t, multiset<size_t>> tempos;
-    size_t maior_d = 0;
+    vector<pair<size_t, size_t>> vacas(n); // litros de leite, paciência
     for (size_t i = 0; i < n; i++) {
-        cin >> l >> d;
-        tempos[d].insert(l);
-        if (tempos[d].size() > d) {
-            tempos[d].erase(tempos[d].begin());
-        }
-        if (maior_d < d) {
-            maior_d = d;
+        cin >> vacas[i].first >> vacas[i].second;
+    }
+    sort(vacas.begin(), vacas.end(),
+            [](pair<size_t, size_t> a, pair<size_t, size_t> b){
+                if (a.second == b.second) {
+                    return a.first > b.first;
+                }
+                return a.second < b.second;
+            }); // (2, 1), (1, 1), (3, 2), (1, 2), (8, 3), (5, 3), ...
+
+    multiset<size_t> leites;
+    for (size_t i = 0; i < n; i++) {
+        if (leites.size() == vacas[i].second) {
+            if (*leites.begin() < vacas[i].first) {
+                leites.erase(leites.begin());
+                leites.insert(vacas[i].first);
+            }
+        } else {
+            leites.insert(vacas[i].first);
         }
     }
-    
-    multiset<size_t> candidatos;
 
-    for (size_t i = 0; i < maior_d; i++) {
-        for (auto& [k, v] : tempos) {
-            auto elem = v.begin()
-        }
-    }
-
-    cout << litros << "\n";
+    cout << accumulate(leites.begin(), leites.end(), 0) << "\n";
 
     return 0;
 }
