2f851b7af36f7ae2762b57de9713885bfca9657d
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-05 02:28:59 -0300

    [week 5] make a prettier

--- semana-5/a.cpp
+++ semana-5/a.cpp
@@ -2,33 +2,20 @@
 using namespace std;
 
 vector<vector<size_t>> dp(3, vector<size_t>(112345, numeric_limits<size_t>::max()));
-vector<size_t> a(112345), b(112345), c(112345);
+vector<vector<size_t>> atividades(3, vector<size_t>(112345));
 size_t n;
 
-size_t sol(int escolhido, size_t i) {
+size_t sol(int e, size_t i) {
     if (i == n-1) {
-        switch (escolhido) {
-            case 0: return dp[0][i] = a[i];
-            case 1: return dp[1][i] = b[i];
-            case 2: return dp[2][i] = c[i];
-        }
+        return dp[e][i] = atividades[e][i];
     }
-    switch (escolhido) {
-        case 0:
-            if (dp[0][i] != numeric_limits<size_t>::max()) {
-                return dp[0][i];
-            }
-            return dp[0][i] = a[i] + max(sol(1, i+1), sol(2, i+1));
-        case 1:
-            if (dp[1][i] != numeric_limits<size_t>::max()) {
-                return dp[1][i];
-            }
-            return dp[1][i] = b[i] + max(sol(0, i+1), sol(2, i+1));
-        case 2:
-            if (dp[2][i] != numeric_limits<size_t>::max()) {
-                return dp[2][i];
-            }
-            return dp[2][i] = c[i] + max(sol(0, i+1), sol(1, i+1));
+    if (dp[e][i] != numeric_limits<size_t>::max()) {
+        return dp[e][i];
+    }
+    switch (e) {
+        case 0: return dp[0][i] = atividades[0][i] + max(sol(1, i+1), sol(2, i+1));
+        case 1: return dp[1][i] = atividades[1][i] + max(sol(0, i+1), sol(2, i+1));
+        case 2: return dp[2][i] = atividades[2][i] + max(sol(0, i+1), sol(1, i+1));
     }
 }
 
@@ -37,7 +24,7 @@ int main() {
 
     cin >> n;
     for (size_t i = 0; i < n; i++) {
-        cin >> a[i] >> b[i] >> c[i];
+        cin >> atividades[0][i] >> atividades[1][i] >> atividades[2][i];
     }
 
     cout << max(max(sol(0, 0), sol(1, 0)), sol(2, 0)) << "\n";
