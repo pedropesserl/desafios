e1ecb09ebb1f6d72f79f053eb25e89d3b8408abd
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-05 02:07:32 -0300

    [week 5] add a (dynamic programming)

--- semana-5/a.cpp
+++ semana-5/a.cpp
@@ -1,37 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* vector<size_t> dp1(112345), dp2(112345), dp3(112345); */
+vector<vector<size_t>> dp(3, vector<size_t>(112345, numeric_limits<size_t>::max()));
+vector<size_t> a(112345), b(112345), c(112345);
+size_t n;
 
-size_t sol(vector<size_t> a, vector<size_t> b, vector<size_t> c, size_t i, int escolhido) {
-    if (i == a.size() - 1) {
+size_t sol(int escolhido, size_t i) {
+    if (i == n-1) {
         switch (escolhido) {
-            case 1: return a[i];
-            case 2: return b[i];
-            case 3: return c[i];
+            case 0: return dp[0][i] = a[i];
+            case 1: return dp[1][i] = b[i];
+            case 2: return dp[2][i] = c[i];
         }
     }
     switch (escolhido) {
-        case 1: return a[i] + max(sol(a, b, c, i+1, 2), sol(a, b, c, i+1, 3));
-        case 2: return b[i] + max(sol(a, b, c, i+1, 1), sol(a, b, c, i+1, 3));
-        case 3: return c[i] + max(sol(a, b, c, i+1, 1), sol(a, b, c, i+1, 2));
+        case 0:
+            if (dp[0][i] != numeric_limits<size_t>::max()) {
+                return dp[0][i];
+            }
+            return dp[0][i] = a[i] + max(sol(1, i+1), sol(2, i+1));
+        case 1:
+            if (dp[1][i] != numeric_limits<size_t>::max()) {
+                return dp[1][i];
+            }
+            return dp[1][i] = b[i] + max(sol(0, i+1), sol(2, i+1));
+        case 2:
+            if (dp[2][i] != numeric_limits<size_t>::max()) {
+                return dp[2][i];
+            }
+            return dp[2][i] = c[i] + max(sol(0, i+1), sol(1, i+1));
     }
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    size_t n;
     cin >> n;
-    vector<size_t> a(n), b(n), c(n);
     for (size_t i = 0; i < n; i++) {
         cin >> a[i] >> b[i] >> c[i];
     }
 
-    /* dp1[0] = a[0]; */
-    /* dp2[0] = b[0]; */
-    /* dp3[0] = c[0]; */
-    cout << max(max(sol(a, b, c, 0, 1), sol(a, b, c, 0, 2)), sol(a, b, c, 0, 3)) << "\n";
+    cout << max(max(sol(0, 0), sol(1, 0)), sol(2, 0)) << "\n";
 
     return 0;
 }
