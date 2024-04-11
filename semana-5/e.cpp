2d7f5e408396c2e1b458cb89eb1d906616b4cd90
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-11 00:56:00 -0300

    [week 5] add e (memory limit exceeded)

--- semana-5/e.cpp
+++ semana-5/e.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<long> a(11234, 0);
+size_t n, c;
 
+vector<vector<long>> dp(11234, vector<long>(11234, numeric_limits<long>::min()));
+
+long sol(size_t ini, size_t k) {
+    if (k * c + ini + 1 > n) {
+        return 0;
+    }
+    if (dp[ini][k] != numeric_limits<long>::min()) {
+        return dp[ini][k];
+    }
+    if (k == 0) { // terminou de posicionar os lapis
+        return dp[ini][k] = accumulate(a.begin() + ini, a.begin() + n, 0);
+    }
+    return dp[ini][k] = max(sol(ini + c, k - 1), a[ini] + sol(ini + 1, k));
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    size_t n, k, c;
+    size_t k;
     cin >> n >> k >> c;
-    vector<long> a(n);
-    for (long i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    long total = accumulate(a.begin(), a.end(), 0);
+    cout << sol(0, k) << "\n";
 
     return 0;
 }
