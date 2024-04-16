0d0541dafff0c3aadd4485db965bccf5f431060d
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-16 19:10:08 -0300

    [week 5] finish f

--- semana-5/f.cpp
+++ semana-5/f.cpp
@@ -3,31 +3,38 @@ using namespace std;
 
 vector<size_t> custos(2000);
 vector<size_t> vendas(2000);
-vector<size_t> min_trocas(2000, numeric_limits<size_t>::max());
-vector<size_t> trocas;
 size_t idade_max, preco_nova;
 
-size_t min_custo(size_t anos_prod, size_t idade) {
-    if (anos_prod == 0) {
-        if (trocas < min_trocas) {
-            min_trocas = trocas;
-        }
-        return 0;
+vector<vector<size_t>> dp(2000+100, vector<size_t>(2000+100, numeric_limits<size_t>::max()));
+vector<vector<size_t>> rec(2000+100, vector<size_t>(2000+100, numeric_limits<size_t>::max()));
+
+int anos_prod;
+
+size_t min_custo(size_t a, size_t idade) {
+    if (a == anos_prod) {
+        return dp[a][idade] = 0;
+    }
+    if (dp[a][idade] != numeric_limits<size_t>::max()) {
+        return dp[a][idade];
     }
     if (idade == idade_max) {
-        trocas.push_back(idade);
-        return -vendas[idade - 1] + preco_nova + custos[0] + min_custo(anos_prod - 1, 1); // substituir
+        rec[a][idade] = 1;
+        return dp[a][idade] = -vendas[idade - 1] + preco_nova + custos[0] + min_custo(a + 1, 1); // substituir
     }
-    trocas.push_back(idade);
-    size_t ret = -vendas[idade - 1] + preco_nova + custos[0] + min_custo(anos_prod - 1, 1); // substituir
-    trocas.pop_back();
-    return min(ret, custos[idade] + min_custo(anos_prod - 1, idade + 1)); // não substituir
+    size_t ret = -vendas[idade - 1] + preco_nova + custos[0] + min_custo(a + 1, 1); // substituir
+    size_t ret2 = custos[idade] + min_custo(a + 1, idade + 1); // não substituir
+    if (ret <= ret2) {
+        rec[a][idade] = 1;
+    } else {
+        rec[a][idade] = idade + 1;
+    }
+    return dp[a][idade] = min(ret, ret2);
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     
-    size_t anos_prod, idade;
+    size_t idade;
     cin >> anos_prod >> idade >> idade_max >> preco_nova;
     for (size_t k = 0; k < idade_max; k++) {
         cin >> custos[k];
@@ -36,15 +43,18 @@ int main() {
         cin >> vendas[k];
     }
 
-    cout << min_custo(anos_prod, idade) << "\n";
-
-    if (trocas.empty()) {
-        cout << "0\n";
-        return 0;
+    cout << min_custo(0, idade) << "\n";
+    bool printed = 0;
+    for (size_t a = 0; a < anos_prod; a++) {
+        if (rec[a][idade] == 1) {
+            cout << a+1 << " ";
+            printed = 1;
+        }
+        idade = rec[a][idade];
     }
-    for (size_t k = 0; k < trocas.size() - 1; k++) {
-        cout << trocas[k] << " ";
+    if (!printed) {
+        cout << 0;
     }
-    cout << trocas[trocas.size() - 1] << "\n";
+    cout << "\n";
     return 0;
 }
