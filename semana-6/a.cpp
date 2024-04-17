ec6428183fa6e4c027575dcb8af64fde84e3016d
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2024-04-16 23:18:35 -0300

    [week 6] start a

---
+++ semana-6/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ideia: manter uma estrutura ordenada, achar a posição a inserir o próximo elemento
+// e o número de desculpas vai ser o tamanho menos a posição mais 1
+
+int main() {
+
+    return 0;
+}
