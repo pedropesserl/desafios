780a24c48652bc54e9ee717d29a25c33941cbdeb
Pedro Pesserl <fpesserl7@gmail.com>
2024-07-01 12:08:30 -0300

    [week 9] add a (slow)

--- semana-9/a.cpp
+++ semana-9/a.cpp
@@ -1,7 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> rede(112345);
+vector<set<int>> rede(112345); // grafo é um vetor de set para poder remover
+                               // conexões facilmente
+vector<bool> visitados(112345, false);
+const long oo = 1987654321987654321;
+vector<long> dists(112345, oo);
+
+void dfs(int v) {
+    visitados[v] = true;
+    for (int u : rede[v]) {
+        if (!visitados[u]) {
+            dfs(u);
+        }
+    }
+}
+
+long dijkstra(int u, int v) {
+    dists.assign(dists.size(), oo);
+    visitados.assign(visitados.size(), false);
+
+    priority_queue<pair<long, int>, vector<pair<long, int>>,
+                   greater<pair<long, int>>> q;
+    dists[u] = 0;
+    q.push({0, u});
+    while (!q.empty()) {
+        auto [d, a] = q.top();
+        q.pop();
+        if (visitados[a]) continue;
+        visitados[a] = true;
+        for (int b : rede[a]) {
+            if (dists[b] > dists[a] + 1) {
+                dists[b] = dists[a] + 1;
+                q.push({dists[b], b});
+            }
+        }
+    }
+
+    return dists[v];
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -11,9 +48,29 @@ int main() {
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        rede[u].push_back(v);
-        rede[v].push_back(u);
+        rede[u].insert(v);
+        rede[v].insert(u);
     }
 
+    int num_componentes = 0;
+    for (int v = 1; v <= n; v++) {
+        if (!visitados[v]) {
+            num_componentes++;
+            dfs(v);
+        }
+    }
+
+    for (int i = 0; i < k; i++) {
+        int u, v;
+        cin >> u >> v;
+        rede[u].erase(v);
+        rede[v].erase(u);
+        if (dijkstra(u, v) == oo) {
+            num_componentes++;
+        }
+        cout << num_componentes << " ";
+    }
+    cout << "\n";
+    
     return 0;
 }
